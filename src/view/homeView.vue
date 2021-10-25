d5d8e0c8303d03df2f66272e0d2fc4e8e9e02a43
Angel Floreano Pisfil <angel.floreano@deister.es>
2021-10-25 23:36:23 +0200

    add home

---
+++ src/view/homeView.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-container fluid>
+        <app-bar/>
+    </v-container>
+</template>
+<script>
+    import appBar from '../components/appBar.vue'
+    export default {
+        name : 'homeView',
+        components :{
+            appBar,
+        }
+
+    }
+</script>

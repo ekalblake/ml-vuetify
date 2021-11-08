907dc9f574ca821bfc98b6062918e9852e584917
paolovillena <paolo.villena@deister.es>
2021-11-08 23:36:30 +0100

    edit navbar

--- src/components/appBar.vue
+++ src/components/appBar.vue
@@ -6,52 +6,80 @@
                 flat
                 height="100"
         >
-          <v-container>
+          <v-container
+              style="font-size:13px;"
+          >
           <v-row no-gutters class="mt-3">
             <v-col cols="2"
-                   class="pr-3 float-right"
             >
-              <v-img class="float-right" src="https://http2.mlstatic.com/frontend-assets/ui-navigation/5.16.2/mercadolibre/logo__large_plus@2x.png" max-width="134" max-height="34"></v-img>
+              <v-img class="float-left" src="https://http2.mlstatic.com/frontend-assets/ui-navigation/5.16.2/mercadolibre/logo__large_plus@2x.png" max-width="134" max-height="34"></v-img>
             </v-col>
-            <v-col cols="5"
+            <v-col cols="6"
             >
               <v-text-field
+                  style="color: rgba(0, 0, 0, 0.87);"
                   solo
                   hide-details
                   label="Buscar productos, marcas y más..."
                   append-icon="mdi-magnify"
-
               ></v-text-field>
 
             </v-col>
-            <v-col cols="5">
-              <v-img class="float-left" src="https://http2.mlstatic.com/D_NQ_684485-MLA47533161749_092021-OO.webp" max-width="340" max-height="39"></v-img>
+            <v-col cols="4">
+              <v-img class="float-right" src="https://http2.mlstatic.com/D_NQ_684485-MLA47533161749_092021-OO.webp" max-width="340" max-height="39"></v-img>
             </v-col>
           </v-row >
+
           <v-row no-gutters>
+            <v-col cols="2">
+              <v-row no-gutters>
+                <v-col cols="3">
+                  <v-btn icon>
+                  <v-icon dark right>
+                    mdi-map-marker
+                  </v-icon>
+                  </v-btn>
+                </v-col>
+                <v-col cols="9">
+                  <p>Enviar a..</p>
+                </v-col>
+              </v-row>
+
+
+            </v-col>
+            <v-col cols="7">
+              <v-card
+                  class="mx-auto"
+                  tile
+                  style="background: none !important; border: none;"
+
+              >
+                <v-list-item>
+                    <v-list-item-content>Categoria</v-list-item-content>
+                    <v-list-item-content>Ofertas</v-list-item-content>
+                    <v-list-item-content>Historial</v-list-item-content>
+                    <v-list-item-content>Tiendas Oficiales</v-list-item-content>
+                    <v-list-item-content>Vender</v-list-item-content>
+                    <v-list-item-content>Ayuda</v-list-item-content>
+                </v-list-item>
+
+              </v-card>
+
+
+            </v-col>
             <v-col cols="3">
-            <v-btn icon>
-              <v-icon dark right>
-                 mdi-map-marker
-              </v-icon>
+              <v-card
+                  class="mx-auto float-right"
+                  tile
+                  style="background: none !important; border: none;"
+              >
+                <v-list-item>
+                  <v-list-item-content>Crea tu cuenta</v-list-item-content>
+                  <v-list-item-content>Ingresa</v-list-item-content>
+                  <v-list-item-content>Mis compras</v-list-item-content>
+                </v-list-item>
 
-            </v-btn>
-
-            </v-col>
-            <v-col cols="4">
-              <v-toolbar-items >
-                    Categoria
-                    Ofertas
-                    Historial
-                    Tiendas Oficiales
-                    Vender
-                    Ayuda
-              </v-toolbar-items>
-
-
-            </v-col>
-            <v-col cols="5">
-
+              </v-card>
             </v-col>
            </v-row>
           </v-container>
--- src/components/carousel.vue
+++ src/components/carousel.vue
@@ -7,15 +7,17 @@
       show-arrows-on-hover
   >
     <v-carousel-item
-        v-for="link in links"
-        :key="link"
+        v-for="(item,i) in items"
+        :key="i"
+        :src="item.src"
+        :href="item.link"
+
     >
         <v-row
             class="fill-height"
             align="center"
             justify="center"
         >
-            <v-img class="float-left" :src="link" max-width="80%"></v-img>
         </v-row>
     </v-carousel-item>
   </v-carousel>
@@ -26,12 +28,27 @@
 export default {
   data: () => ({
     model: 0,
-    links: [
-      'https://http2.mlstatic.com/D_NQ_809090-MLA48143355195_112021-OO.webp',
-      'https://http2.mlstatic.com/D_NQ_807957-MLA48143749607_112021-OO.webp',
-      'https://http2.mlstatic.com/D_NQ_616314-MLA48143894079_112021-OO.webp',
-      'https://http2.mlstatic.com/D_NQ_676290-MLA47591642747_092021-OO.webp',
-      'https://http2.mlstatic.com/D_NQ_979957-MLA48143894238_112021-OO.webp',
+    items: [
+      {
+        src:'https://http2.mlstatic.com/D_NQ_809090-MLA48143355195_112021-OO.webp',
+        link:'https://ofertas.mercadolibre.com.pe/especial-celulares-2021#me.position=0&me.bu_line=20&me.flow=-1&me.bu=3&me.audience=all&me.content_id=MS_WEB_ESPCELULARES2021_W46&me.component_id=main_slider_web_ml_0&me.logic=user_journey&audience=all&bu=3&bu_line=20&component_id=main_slider_web_ml_0&content_id=MS_WEB_ESPCELULARES2021_W46&flow=-1&logic=user_journey&position=0&c_id=/home/exhibitors-carousel/element&c_campaign=especial-celulares-2021&c_element_order=1&c_uid=85f59b32-ed37-4bd7-9f1e-2eb56dd00de9'
+      },
+      {
+        src:'https://http2.mlstatic.com/D_NQ_807957-MLA48143749607_112021-OO.webp',
+        link:'https://ofertas.mercadolibre.com.pe/especial-juguetes-y-bebes-nov21#me.bu=3&me.audience=all&me.content_id=MS_WEB_ESPJUGUETESYBEBESNOV21_W46&me.component_id=main_slider_web_ml_1&me.logic=user_journey&me.position=1&me.bu_line=16&me.flow=-1&audience=all&bu=3&bu_line=16&component_id=main_slider_web_ml_1&content_id=MS_WEB_ESPJUGUETESYBEBESNOV21_W46&flow=-1&logic=user_journey&position=1&c_id=/home/exhibitors-carousel/element&c_campaign=especial-juguetes-y-bebes-nov21&c_element_order=2&c_uid=a3fb3f28-359e-4f3c-bb10-13398bfc3814'
+      },
+      {
+        src:'https://http2.mlstatic.com/D_NQ_616314-MLA48143894079_112021-OO.webp',
+        link:'https://ofertas.mercadolibre.com.pe/especial-mabe#me.bu_line=20&me.flow=-1&me.bu=3&me.audience=all&me.content_id=MS_WEB_ESPMABE_W46&me.component_id=main_slider_web_ml_2&me.logic=user_journey&me.position=2&audience=all&bu=3&bu_line=20&component_id=main_slider_web_ml_2&content_id=MS_WEB_ESPMABE_W46&flow=-1&logic=user_journey&position=2&c_id=/home/exhibitors-carousel/element&c_campaign=especial-mabe&c_element_order=3&c_uid=4b3434f9-102b-430c-afde-f4091e7be490'
+      },
+      {
+        src:'https://http2.mlstatic.com/D_NQ_676290-MLA47591642747_092021-OO.webp',
+        link:'https://www.mercadolibre.com.pe/ofertas?free_shipping=1#origin=qcat&filter_applied=free_shipping&filter_position=4&me.bu_line=26&me.flow=-1&me.bu=3&me.audience=all&me.content_id=MS_WEB_FREESHIPPING_W42&me.component_id=main_slider_web_ml_3&me.logic=user_journey&me.position=3&audience=all&bu=3&bu_line=26&component_id=main_slider_web_ml_3&content_id=MS_WEB_FREESHIPPING_W42&flow=-1&logic=user_journey&position=3&c_id=/home/exhibitors-carousel/element&c_campaign=ofertas%3Ffree_shipping%3D1&c_element_order=4&c_uid=02011d75-bf4e-41ee-aebf-3b3f7f657f81'
+      },
+      {
+        src:'https://http2.mlstatic.com/D_NQ_979957-MLA48143894238_112021-OO.webp',
+        link:'https://ofertas.mercadolibre.com.pe/especial-belleza-y-salud-nov#me.bu_line=13&me.flow=-1&me.bu=3&me.audience=all&me.content_id=MS_WEB_ESPBELLEZAYSALUDNOV_W46&me.component_id=main_slider_web_ml_4&me.logic=user_journey&me.position=4&audience=all&bu=3&bu_line=13&component_id=main_slider_web_ml_4&content_id=MS_WEB_ESPBELLEZAYSALUDNOV_W46&flow=-1&logic=user_journey&position=4&c_id=/home/exhibitors-carousel/element&c_campaign=especial-belleza-y-salud-nov&c_element_order=5&c_uid=309b34f4-1ffb-4ae8-906b-e0961b00c71d'
+      }
     ],
   }),
 }

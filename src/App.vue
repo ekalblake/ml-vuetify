af54f02cf6b9d1f8c51a815d8350f853ca987023
paolovillena <paolo.villena@deister.es>
2021-11-08 20:58:03 +0100

    Revert "Revert "add carousel and navbar""
    This reverts commit 2fe337465dc79a68687438c24a10899fe5e76612.

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <v-app>
     <v-main>
         <home-view/>
+        <carousel/>
     </v-main>
   </v-app>
 </template>
 
 <script>
 import homeView from '/src/view/homeView.vue'
+import carousel from '../src/components/carousel.vue'
 
 export default {
   name: 'App',
 
   components: {
-      homeView
+      homeView,
+      carousel,
   },
 
   data: () => ({
---
+++ src/components/carousel.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-carousel
+      v-model="model"
+      cycle
+      height="338"
+      hide-delimiter-background
+      show-arrows-on-hover
+  >
+    <v-carousel-item
+        v-for="link in links"
+        :key="link"
+    >
+        <v-row
+            class="fill-height"
+            align="center"
+            justify="center"
+        >
+            <v-img class="float-left" :src="link" max-width="80%"></v-img>
+        </v-row>
+    </v-carousel-item>
+  </v-carousel>
+</template>
+
+
+<script>
+export default {
+  data: () => ({
+    model: 0,
+    links: [
+      'https://http2.mlstatic.com/D_NQ_809090-MLA48143355195_112021-OO.webp',
+      'https://http2.mlstatic.com/D_NQ_807957-MLA48143749607_112021-OO.webp',
+      'https://http2.mlstatic.com/D_NQ_616314-MLA48143894079_112021-OO.webp',
+      'https://http2.mlstatic.com/D_NQ_676290-MLA47591642747_092021-OO.webp',
+      'https://http2.mlstatic.com/D_NQ_979957-MLA48143894238_112021-OO.webp',
+    ],
+  }),
+}
+</script>

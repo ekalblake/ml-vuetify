c28f3d40654037bc7d1a3d337d5c2d95173892a0
paolovillena <paolo.villena@deister.es>
2021-11-11 15:11:13 +0100

    create footer, edit home, edit product store and edit navbar

--- src/components/ProductStore.vue
+++ src/components/ProductStore.vue
@@ -1,50 +1,54 @@
 <template>
-  <v-card
-      class="mx-auto"
-      max-width="300"
-      max-height="360"
-  >
-    <v-img
-        src="	https://http2.mlstatic.com/storage/official-stores-images/sony/background_home201803270119.jpg"
-        height="100px"
-    ></v-img>
-    <v-list-item-avatar
-        size="80"
-        style="margin-top:-45px; margin-left:90px"
-        rounded
+  <v-item v-slot="{toggle }">
+    <v-card
+        class="mx-auto"
+        max-width="300"
+        max-height="360"
+        @click="toggle"
+        href="/"
     >
       <v-img
-          src="https://http2.mlstatic.com/D_Q_NP_742348-MLA25765809298_072017-AE.webp"
+          src="	https://http2.mlstatic.com/storage/official-stores-images/sony/background_home201803270119.jpg"
           height="100px"
       ></v-img>
-    </v-list-item-avatar>
-    <v-card-title class="justify-center">
-      Sony
-    </v-card-title>
-    <v-card-subtitle>
-      <v-item-group multiple>
-        <v-row>
-          <v-col
-              v-for="n in 3"
-              :key="n"
-              cols="12"
-              md="4"
-          >
-              <v-card
+      <v-list-item-avatar
+          size="80"
+          style="margin-top:-45px; margin-left:100px"
+          rounded
+      >
+        <v-img
+            src="https://http2.mlstatic.com/D_Q_NP_742348-MLA25765809298_072017-AE.webp"
+        ></v-img>
+      </v-list-item-avatar>
+      <v-card-title class="justify-center" style="margin-top:-20px">
+        Sony
+      </v-card-title>
+      <v-card-subtitle>
+        <v-item-group multiple>
+          <v-row class="justify-center">
+            <v-col
+                v-for="n in 3"
+                :key="n"
+                cols="12"
+                md="4"
+                sm="4"
+                xs="2"
+            >
+                <v-card
 
-                  class="d-flex align-center"
-                  dark
-                  height="70"
-              >
-                <v-img src="https://http2.mlstatic.com/D_Q_NP_615105-MPE43080791425_082020-R.webp"></v-img>
-              </v-card>
-          </v-col>
-        </v-row>
-      </v-item-group>
-    </v-card-subtitle>
+                    class="d-flex align-center"
+                    height="70"
+                >
+                  <v-img src="https://http2.mlstatic.com/D_Q_NP_615105-MPE43080791425_082020-R.webp"></v-img>
 
-
-  </v-card>
+                </v-card>
+            </v-col>
+            <p>Ver tienda</p>
+          </v-row>
+        </v-item-group>
+      </v-card-subtitle>
+    </v-card>
+  </v-item>
 </template>
 
 
--- src/view/homeView.vue
+++ src/view/homeView.vue
@@ -1,36 +1,110 @@
 <template>
-  <div>
+  <div style="background-color: #ebebeb">
     <app-bar/>
     <carousel/>
-    <v-container>
-      <v-card
-      class="mt-10 px-10"
-      maxwidth="100%"
-      height="90px"
-      >
-        <v-row no-gutters class="pt-3">
-          <v-col cols="3">
-            <p style="font-size:18px;">Paga cómodo y seguro</p>
-            <span style="font-size:14px;color: #999;">Con Mercado Pago</span>
-          </v-col>
-          <v-col cols="3">
-            <p style="font-size:18px;">Hasta 12 cuotas sin interés con:</p>
-            <span style="font-size:14px;color: #3483fa">Ver condiciones</span>
-          </v-col>
-          <v-col cols="2">
-            <v-img src="https://http2.mlstatic.com/secure/payment-logos/v2/payment-logos-mpe-large_v_91cc43d5a1.png" max-width="66px" max-height="20px"></v-img>
-          </v-col>
-          <v-col cols="2">
-            <v-img src="https://http2.mlstatic.com/secure/payment-logos/v2/payment-logos-mpe-large_v_91cc43d5a1.png" max-width="66px" max-height="20px"></v-img>
-            </v-col>
-          <v-col cols="2">
-            <p>Más metodos de pago</p>
-            <span style="font-size:14px;color: #3483fa">Ver Todos</span>
-          </v-col>
-        </v-row>
-      </v-card>
+      <v-container>
+          <v-card
+          class="mt-5 px-10"
+          maxwidth="100%"
+          height="90px"
+          style="border-left:5px solid #333; border-color: #3483fa"
+          >
+            <v-row no-gutters class="pt-3" >
+              <v-col cols="3" >
+                <p style="font-size:18px;">Paga cómodo y seguro</p>
+                <span style="font-size:14px;color: #999;">Con Mercado Pago</span>
+              </v-col>
+              <v-divider vertical></v-divider>
+              <v-col cols="3">
+                <p style="font-size:18px;">Hasta 12 cuotas sin interés con:</p>
+                <div>
+                  <v-dialog
+                      v-model="dialog"
+                      width="825"
+                  >
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-btn
+                          text
+                          v-bind="attrs"
+                          v-on="on"
+                          style="font-size:14px;color: #3483fa;$btn-text-transform: lowercase !important;"
 
-    </v-container>
+                      >
+                        Ver condiciones
+                      </v-btn>
+                    </template>
+
+                    <v-card>
+                      <v-card-title class="text-h5 grey lighten-2">
+                       Condiciones Generales
+                      </v-card-title>
+                      <v-row>
+                        <v-col cols="12"
+                               md="6">
+                          <v-img src="https://www.bbva.com/wp-content/uploads/2019/04/Logo-BBVA-1024x576.jpg"   max-width="66px" max-height="20px"></v-img>
+                          <v-card-text>
+                            Hasta 12 cuotas sin interés
+<v-spacer></v-spacer>
+                            Visa y Mastercard
+                          </v-card-text>
+                        </v-col>
+                        <v-col cols="12"
+                               md="6">
+
+                          <h2>BBVA</h2>
+                          <v-card-text style="font-size:10px">
+                            Pagos Sin Intereses es un Programa que aplica exclusivamente a las tarjetas de crédito VISA y Mastercard del BBVA Continental.
+                            No aplica para tarjetas empresariales (solo aplica para la Tarjeta Capital de Trabajo del BBVA Continental).
+                            Aplica para compras realizadas a través de Mercado Pago en www.MercadoLibre.com.pe y para productos seleccionados (Publicaciones Premium).
+                            El Usuario debe verificar que al momento de hacer el pago la cantidad de cuotas seleccionada debe contener la frase Sin Interés, si no aparece esta leyenda es porque el producto no aplica a dicho beneficio.
+                            El cliente debe elegir un número de cuotas hasta 3, 6, 9 o 12 cuotas. En caso el número de cuotas seleccionado sea mayor a los indicados se aplicará la tasa de interés que corresponda según lo que el usuario tenga contratado con el banco.
+
+                          </v-card-text>
+                        </v-col>
+                      </v-row>
+                      <v-row>
+                        <v-col cols="12"
+                               md="6">
+                          <v-img  src="https://cdn.worldvectorlogo.com/logos/diners-club-logo3-1.svg" max-width="116px" max-height="30px" ></v-img>
+                          <v-card-text>
+                            Hasta 12 cuotas sin interés
+<v-spacer></v-spacer>
+                            Visa y Mastercard
+                          </v-card-text>
+                        </v-col>
+                        <v-col cols="12"
+                               md="6">
+
+                          <h2>Diners</h2>
+                          <v-card-text style="font-size:10px">
+                            Cuotas Sin Intereses es un beneficio permanente que aplica exclusivamente para compras con las tarjetas Diners Club emitidas en el Perú.
+                            Aplica para compras realizadas a través de la web de www.mercadolibre.com.pe para productos seleccionados (Publicaciones Premium).
+                            No aplica para tarjetas Diners Club Corporate. El usuario debe verificar que al momento de hacer el pago la cantidad de cuotas seleccionada debe contener la frase Sin Interés, si no aparece esta leyenda es porque el producto no aplica a dicho beneficio.
+                            Considerar que el valor mínimo de cada cuota, debe ser de S/ 10.00 o U$$ 5.00 de lo contrario la transacción será¡ rechazada.
+                            El cliente debe elegir un número de cuotas entre 02 hasta 06 cuotas, en caso el número de cuotas seleccionado sea mayor a los indicados se aplicará la tasa de interés que corresponda según el tarifario vigente desde la primera cuota.
+                            Ver en https://www.dinersclub.pe/portal/tarifas/tasas-y-tarifas.
+                          </v-card-text>
+                        </v-col>
+                      </v-row>
+                      <v-divider></v-divider>
+                    </v-card>
+                  </v-dialog>
+                </div>
+
+              </v-col>
+              <v-col cols="2" class="align-center">
+                <v-img src="https://www.bbva.com/wp-content/uploads/2019/04/Logo-BBVA-1024x576.jpg"   max-width="66px" max-height="20px"></v-img>
+               </v-col>
+              <v-col cols="2">
+                <v-img  src="https://cdn.worldvectorlogo.com/logos/diners-club-logo3-1.svg" max-width="116px" max-height="30px" ></v-img>
+                </v-col>
+              <v-col cols="2">
+                <p>Más metodos de pago</p>
+                <a href="/" style="font-size:14px;color: #3483fa">Ver Todos</a>
+              </v-col>
+            </v-row>
+          </v-card>
+      </v-container>
     <v-container>
       <v-card-title>Ofertas</v-card-title>
       <v-row
@@ -60,7 +134,7 @@
 
     </v-container>
     <v-container class="mt-5">
-      <v-card-title>Las mejores tiendas te esperan</v-card-title>
+      <v-card-title style="color:#666">Las mejores tiendas te esperan</v-card-title>
       <v-row
       >
         <v-col v-for="(store,i) in productstore"
@@ -74,6 +148,22 @@
       </v-row>
 
     </v-container>
+
+    <v-container class="mt-5 mb-5">
+      <v-card-title>Te puede interesar..</v-card-title>
+      <v-row
+      >
+        <v-col v-for="(disc,i) in productdisc"
+               :key="i"
+               cols="12"
+               md="6"
+        >
+          <product-discounts/>
+        </v-col>
+      </v-row>
+
+    </v-container>
+    <footer-bar/>
   </div>
 </template>
 <script>
@@ -82,6 +172,7 @@
     import productOffer from '../components/ProductOffer.vue'
     import productDiscounts from '../components/ProductDiscounts.vue'
     import productsStore from '../components/ProductStore.vue'
+    import footerBar from '../components/footerBar.vue'
 
     export default {
         name : 'homeView',
@@ -90,9 +181,11 @@
           carousel,
           productOffer,
           productDiscounts,
-          productsStore
+          productsStore,
+          footerBar
         },
         data: () => ({
+            dialog:false,
             products:[
               {
                 title: 'Producto1',
